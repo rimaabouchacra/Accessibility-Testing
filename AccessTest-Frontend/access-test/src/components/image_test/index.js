32fe290a4f0e66286e60c01213c2a59d79222fa7
Reema224 <rima.abouchakra.gms@gmail.com>
2023-05-22 18:37:00 +0300

    added possible image test failure

--- AccessTest-Frontend/access-test/src/components/image_test/index.js
+++ AccessTest-Frontend/access-test/src/components/image_test/index.js
@@ -7,6 +7,7 @@ const TestImage = () => {
   return (
     <div>
       <h1 className='test-title'>Test1: Image Testing</h1>
+      <h2 className='test-def'>Definition</h2>
       <div className='paragraph-img'>
         <p className='def-paragraph'>Images and graphics make content more pleasant and easier to understand for many people, and in particular for those with cognitive and learning disabilities. They serve as cues that are used by people with visual impairments, including people with low vision, to orient themselves in the content.<br/> Images must have text alternatives that describe the information or function represented by them. This ensures that images can be used by people with various disabilities. Let’s see how to provide appropriate text alternatives based on the purpose of the image. There are 7 types of images, each should have a specific alternative.</p>
         <img className='img-test' src={imageTest} alt="wrong and right alt text" />
@@ -20,6 +21,7 @@ const TestImage = () => {
         <li><b>Groups of images: </b>If multiple images convey a single piece of information, the text alternative for one image should convey the information for the entire group.</li>
         <li><b>Image maps: </b>The text alternative for an image that contains multiple clickable areas should provide an overall context for the set of links.</li>
       </ol>
+      <h2 className='test-fail'>Possible Failure</h2>
     </div>
   );
 };
